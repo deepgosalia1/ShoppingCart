dae1e93ba54692b304234d06bcf3b0e2d7eb0406
Deep Gosalia <32708932+deepgosalia1@users.noreply.github.com>
2020-08-26 01:21:04 +0530

    Updates

---
+++ precache-manifest.4505185ec2fc48de7fc7badf8992a0fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd25d6e22d4276256fdbc7c10badb96c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5235edb12b975d90ee24",
+    "url": "/static/css/main.28f43278.chunk.css"
+  },
+  {
+    "revision": "e3ff252e6f1c61ec4bff",
+    "url": "/static/js/2.6a7ccfd1.chunk.js"
+  },
+  {
+    "revision": "729937898630e839994162d5d9f8ce89",
+    "url": "/static/js/2.6a7ccfd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5235edb12b975d90ee24",
+    "url": "/static/js/main.cf41a01b.chunk.js"
+  },
+  {
+    "revision": "16985de409df605dd7e9",
+    "url": "/static/js/runtime-main.68d2d271.js"
+  }
+]);
