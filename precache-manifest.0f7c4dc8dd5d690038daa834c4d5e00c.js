3b4841f91d061500fb8ae1aaa974c2be2d289ba6
Deep Gosalia <32708932+deepgosalia1@users.noreply.github.com>
2020-08-26 01:22:40 +0530

    Updates

---
+++ precache-manifest.0f7c4dc8dd5d690038daa834c4d5e00c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d39423f855dcecbe1b1468b1ae7fc58f",
+    "url": "/ShoppingCart/index.html"
+  },
+  {
+    "revision": "6c1746d96000069320ab",
+    "url": "/ShoppingCart/static/css/main.28f43278.chunk.css"
+  },
+  {
+    "revision": "e3ff252e6f1c61ec4bff",
+    "url": "/ShoppingCart/static/js/2.6a7ccfd1.chunk.js"
+  },
+  {
+    "revision": "729937898630e839994162d5d9f8ce89",
+    "url": "/ShoppingCart/static/js/2.6a7ccfd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c1746d96000069320ab",
+    "url": "/ShoppingCart/static/js/main.a7ceff40.chunk.js"
+  },
+  {
+    "revision": "8dda23fc3ee2796f4642",
+    "url": "/ShoppingCart/static/js/runtime-main.53f57bf2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4505185ec2fc48de7fc7badf8992a0fc.js"
+  "/ShoppingCart/precache-manifest.0f7c4dc8dd5d690038daa834c4d5e00c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ShoppingCart/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
